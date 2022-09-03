475b13fc2ecd3f09b2425dda22a82fb7c565f30d
Nyth797 <Nyth797@gmail.com>
2022-09-03 17:14:30 +0200

    deploy final??plz

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(session({
     cookie: { 
         maxAge: 1000 * 60 * 60 * 24 * 14, /* 14 days */
         HttpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: true
     }
 }));
