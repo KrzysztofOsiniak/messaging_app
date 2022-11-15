f6558c49bc1f23cf7ed3bbcb748f3e8e74e7c529
Nyth797 <Nyth797@gmail.com>
2022-11-15 22:30:40 +0100

    added change chat button

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.disable('x-powered-by');
 
 const limiter = rateLimit({
 	windowMs: 2 * 60 * 1000, // 2 minutes
-	max: 50, // Limit each IP to 50 requests per `window` (here, per 2 minutes)
+	max: 500, // Limit each IP to 50 requests per `window` (here, per 2 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
