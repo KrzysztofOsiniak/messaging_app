0097246cd45479617bfb51521b5f113bb4c003f4
Nyth797 <Nyth797@gmail.com>
2023-12-23 21:45:44 +0100

    added session.userId, rem changes

--- controllers/users.js
+++ controllers/users.js
@@ -14,14 +14,14 @@ export const postlogin = async (req, res) => {
         const username = req.body.username;
         const password = req.body.password;
         if( ((password.length > 29) || (username.length > 19)) || (!password.length || !username.length) ) {
-            req.session.destroy()
+            req.session.destroy();
             res.sendStatus(400);
             return
         }
         const user = await db.promise().execute(`SELECT * FROM USERS WHERE USERNAME = ?;`, [username])
         .catch(err => {
             console.error(err);
-            req.session.destroy()
+            req.session.destroy();
             res.sendStatus(500);
             return null
         });
@@ -29,27 +29,18 @@ export const postlogin = async (req, res) => {
             return
         }
         if(!user[0][0]) {
-            req.session.destroy()
+            req.session.destroy();
             res.sendStatus(400);
             return
         }
-        const hash = await db.promise().execute(`SELECT password FROM USERS WHERE USERNAME = ?;`, [username])
-        .catch(err => {
-            console.error(err);
-            req.session.destroy()
-            res.sendStatus(500);
-            return null
-        });
-        if(hash === null) {
-            return
-        }
-        const result = await bcrypt.compare(`${password}`, `${hash[0][0].password}`)
+        const hash = user[0][0].password;
+        const result = await bcrypt.compare(`${password}`, `${hash}`)
         .then(result => {
             return result
         })
         .catch(err => {
             console.error(err);
-            req.session.destroy()
+            req.session.destroy();
             res.sendStatus(500);
             return null
         });
@@ -57,15 +48,16 @@ export const postlogin = async (req, res) => {
             return
         }
         if(!result) {
-            req.session.destroy()
+            req.session.destroy();
             res.sendStatus(400);
             return
-        } 
+        }
+        req.session.userId = user[0][0].id;
         req.session.logged = true;
-        req.session.username = user[0][0].username;
+        req.session.username = username;
         req.session.pending = false;
         res.status(200).send({username: req.session.username, status: 200})
-    }, 350);
+    }, 350)
 };
 
 
@@ -114,17 +106,20 @@ export const postsignup = (req, res) => {
         if(hash === null) {
             return
         }
-        db.execute(`INSERT INTO USERS(username, password) VALUES(?, ?);`, [username, hash], async (err) => {
-            if(err) {
-                console.error(err);
-                req.session.destroy()
-                res.sendStatus(500);
-                return
-            }
-            req.session.logged = true;
-            req.session.username = user[0][0].username;
-            req.session.pending = false;
-            res.status(200).send({username: req.session.username, status: 200})
+        const result = await db.promise().execute(`INSERT INTO USERS(username, password) VALUES(?, ?);`, [username, hash])
+        .catch(err => {
+            console.error(err);
+            req.session.destroy()
+            res.sendStatus(500);
+            return null
         });
+        if(result === null) {
+            return
+        }
+        req.session.userId = result[0].insertId;
+        req.session.logged = true;
+        req.session.username = username;
+        req.session.pending = false;
+        res.status(200).send({username: req.session.username, status: 200})
     }, 350);
 };
